6dfae5bebbd790fc8a659c54ae6d35f85f474247
aimeeaidanu <81179247+aimeeaidanu@users.noreply.github.com>
2022-01-09 18:49:12 -0800

    Main files
    don't copy without my permission pls.

--- Main/site.php
+++ Main/site.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php include "RealNavBar.html" ?>
+    <?php include "Welcome.html" ?>
+    <?php include "GoFundMe.html" ?>
+    <?php include "AboutMe.html" ?>
+    <?php include "contact.html" ?>
+  </body>
+</html>
